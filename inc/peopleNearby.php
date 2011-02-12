35c5adcf0e392db8084fa1cf9f1e72418027814b
Jason Grant <jason@flexewebs.com>
2011-02-12 12:35:37 +0000

    People nearby placeholder

--- inc/peopleNearby.php
+++ inc/peopleNearby.php
@@ -1,7 +1,12 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-?>
+<div id="peopleNearby">
+    <h2>People near you</h2>
+    <ul>
+        <?php
+        for ($i=0; $i<10; $i++) {
+        ?>
+        <li><img src="i/profiles/<?php echo $i;?>.jpg" /></li>
+        <?php
+        }
+        ?>
+    </ul>
+</div>
